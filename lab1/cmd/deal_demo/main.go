d0f3d761509191b0408eef5d8368a6397de51b95
NikitaKoros <nikitakorostin09@gmail.com>
2025-11-21 16:48:38 +0300

    new main for deal

--- lab1/cmd/deal_demo/main.go
+++ lab1/cmd/deal_demo/main.go
@@ -9,13 +9,10 @@ import (
 )
 
 func main() {
-	// Создаем базовый DES
 	desCipher := des.NewDES()
 
-	// Создаем DEAL-128 напрямую (альтернатива фабрике)
 	dealCipher := deal.NewDEAL128(desCipher)
 
-	// Устанавливаем ключ (128 бит = 16 байт)
 	key := make([]byte, 16)
 	for i := range key {
 		key[i] = byte(i)
@@ -25,7 +22,6 @@ func main() {
 		log.Fatal("SetEncryptionKey error:", err)
 	}
 
-	// Шифруем блок (128 бит = 16 байт)
 	block := make([]byte, 16)
 	for i := range block {
 		block[i] = byte(i + 10)
@@ -40,7 +36,6 @@ func main() {
 
 	fmt.Printf("Encrypted: %x\n", encrypted)
 
-	// Дешифруем
 	decrypted, err := dealCipher.DecryptBlock(encrypted)
 	if err != nil {
 		log.Fatal("DecryptBlock error:", err)
@@ -48,6 +43,5 @@ func main() {
 
 	fmt.Printf("Decrypted: %x\n", decrypted)
 
-	// Проверяем совпадение
 	fmt.Printf("Match: %t\n", string(block) == string(decrypted))
 }
